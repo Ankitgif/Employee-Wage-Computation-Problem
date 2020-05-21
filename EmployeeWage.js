be0f240a286da298d369773c8f849009b6bc6309
Ankitgif <ankitsinha2303@gmail.com>
2020-05-21 18:52:18 +0530

    [Ankit] Added case statement instead of if-else

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,18 +3,18 @@ let isFullTime = 1;
 let isPartTime = 2;
 let EMP_WAGE_PER_HR = 20;
 let empCheck = Math.floor(Math.random() * 3);
-if(isFullTime == empCheck){
-    console.log("Present");
-    let fullDayHr = 8;
-    let empWage = EMP_WAGE_PER_HR * fullDayHr;
-    console.log("Employee Wage :",empWage);
-}else if(isPartTime == empCheck){
-    console.log("Part Time Employee");
-    let partTimeHr = 4;
-    let empWage = partTimeHr * EMP_WAGE_PER_HR;
-    console.log("Employee Wage of Part Time Employee :",empWage);
-}else{
-    console.log("Absent");
-    let empWage = 0;
-    console.log("Employee Wage :",empWage);
+switch(empCheck){
+    case isFullTime:
+        console.log("Full Time Employee");
+        empHr = 8;
+        break;
+    case isPartTime:
+        console.log("Part Time Employee");
+        empHr = 4;
+        break;
+    default:
+        console.log("Employee is Absent");
+        empHr = 0;           
 }
+let empWage = EMP_WAGE_PER_HR * empHr;
+console.log("Employee Wage :",empWage);
