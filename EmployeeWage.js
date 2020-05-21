0025d298960b9b4ff15b26812a960ae9235b6fd0
Ankitgif <ankitsinha2303@gmail.com>
2020-05-21 22:55:44 +0530

    [Ankit] Added code for calculating wages till a condition of total working hours or days is reached for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,26 +1,29 @@
 console.log("Welcome to Employee Wage Computation Program");
-let isFullTime = 1;
-let isPartTime = 2;
+let IS_FULL_TIME = 1;
+let IS_PART_TIME = 2;
 let EMP_WAGE_PER_HR = 20;
+let MAX_HR_IN_MONTH = 100;
 let totalWage = 0;
-let noOfWorkingDays = 20;
-for(let day = 1; day <= noOfWorkingDays; day++){
+let NUM_OF_WORKING_DAYS = 20;
+let totalEmpHr = 0;
+let totalWorkingDays = 0;
+while(totalEmpHr <= MAX_HR_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 3);
     switch(empCheck){
-        case isFullTime:
+        case IS_FULL_TIME:
             console.log("Full Time Employee");
             empHr = 8;
             break;
-        case isPartTime:
+        case IS_PART_TIME:
             console.log("Part Time Employee");
             empHr = 4;
             break;
         default:
             console.log("Employee is Absent");
             empHr = 0;           
-        }
-    let empWage = EMP_WAGE_PER_HR * empHr;
-    console.log("Employee Wage :",empWage);
-    totalWage = totalWage + empWage;
-    console.log("Total Wage of Employee :",totalWage);
+    }
+    totalEmpHr = totalEmpHr + empHr;
 }
+totalWage = totalEmpHr * EMP_WAGE_PER_HR;
+console.log("Total wage of employee :",totalWage);
