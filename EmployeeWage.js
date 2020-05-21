91b7e1c7e33962efad1af2350a6298a79b840496
Ankitgif <ankitsinha2303@gmail.com>
2020-05-21 14:23:15 +0530

    [Ankit] Added code for checking employee attendance

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,8 @@
 console.log("Welcome to Employee Wage Computation Program");
+let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 2);
+if(isPresent <= empCheck){
+    console.log("Present");
+}else{
+    console.log("Absent");
+}
