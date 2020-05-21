6cc26c3167951b9b9171ff74b2114913f7f24545
Ankitgif <ankitsinha2303@gmail.com>
2020-05-21 18:27:14 +0530

    [Ankit] Added part time employee and wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,12 +1,18 @@
 console.log("Welcome to Employee Wage Computation Program");
 let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 2);
-if(isPresent <= empCheck){
+let isPartTime = 2;
+let EMP_WAGE_PER_HR = 20;
+let empCheck = Math.floor(Math.random() * 3);
+if(isPresent == empCheck){
     console.log("Present");
-    let empWagePerHr = 20;
     let fullDayHr = 8;
-    let empWage = empWagePerHr * fullDayHr;
+    let empWage = EMP_WAGE_PER_HR * fullDayHr;
     console.log("Employee Wage :",empWage);
+}else if(isPartTime == empCheck){
+    console.log("Part Time Employee");
+    let partTimeHr = 4;
+    let empWage = partTimeHr * EMP_WAGE_PER_HR;
+    console.log("Employee Wage of Part Time Employee :",empWage);
 }else{
     console.log("Absent");
     let empWage = 0;
