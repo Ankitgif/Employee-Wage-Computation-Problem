2c74e74352c9e45e62adee10ae7c3d56adcdd6ea
Ankitgif <ankitsinha2303@gmail.com>
2020-05-21 14:44:45 +0530

    [Ankit] Added code for calculating daily employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,6 +3,12 @@ let isPresent = 1;
 let empCheck = Math.floor(Math.random() * 2);
 if(isPresent <= empCheck){
     console.log("Present");
+    let empWagePerHr = 20;
+    let fullDayHr = 8;
+    let empWage = empWagePerHr * fullDayHr;
+    console.log("Employee Wage :",empWage);
 }else{
     console.log("Absent");
+    let empWage = 0;
+    console.log("Employee Wage :",empWage);
 }
